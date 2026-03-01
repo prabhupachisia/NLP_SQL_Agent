c641757602c384d132687f10f5161761b1f225dd
prabhupachisia <prabhu.pachisia2023@vitstudent.ac.in>
2026-03-01 12:34:10 +0530

    Maded changes in databse location

--- backend/config.py
+++ backend/config.py
@@ -1,12 +1,15 @@
 import os
 from dotenv import load_dotenv
 
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+
 load_dotenv()
 
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY", "fallback_secret_key")
     ENCRYPTION_KEY = os.getenv("ENCRYPTION_KEY")
     GROQ_API_KEY = os.getenv("GROQ_API_KEY")
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "sqlite:///app.db")
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", 
+        "sqlite:///" + os.path.join(BASE_DIR, "app.db"))
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_EXPIRY = int(os.getenv("JWT_EXPIRY", 86400))
